d127837056999e09978f67c142e0d8b4c3ffb4f2
thibault-roux <thibault.roux@uclouvain.be>
2025-01-23 10:53:38 +0100

    Cleaner code

--- infer.py
+++ infer.py
@@ -1,6 +1,7 @@
 from ollama import chat
 from ollama import ChatResponse
 import pandas as pd
+import progressbar
 
 # Function to classify text difficulty
 def classify_text_difficulty(text: str) -> str:
@@ -27,6 +28,22 @@ def load_dataset(path="../../data/Qualtrics_Annotations_formatB.csv"):
     df = pd.read_csv(path)
     return df
 
+def infer_classification(dataset):
+    # dataset["difficulty"] = dataset["text"].apply(classify_text_difficulty)
+    # this time let's classify difficulty level with a progressbar
+    bar = progressbar.ProgressBar(maxval=len(dataset))
+    i = 0
+    for index, row in dataset.iterrows():
+        row["difficulty"] = classify_text_difficulty(row["text"])
+        i += 1
+        bar.update(i)
+    bar.finish()
+
+    # save in csv format
+    dataset.to_csv("./data/Qualtrics_Annotations_formatB_out.csv", index=False)
+
+    return dataset
+
 
 if __name__ == "__main__":
     text_to_classify = "Ce n'est pas l'insecte, mais un petit morceau de velours ou de taffetas noir, rond et plat ressemblant à un grain de beauté que les femmes coquettes de la haute société se collaient sur le visage ou sur le décolleté pour mettre en valeur la blancheur de leur teint ou la perfection d'une partie de leur personne."
@@ -34,7 +51,7 @@ if __name__ == "__main__":
 
     dataset = load_dataset() # csv Qualtrics_Annotations_formatB_in.csv
     print(dataset)
-    dataset["difficulty"] = dataset["text"].apply(classify_text_difficulty)
+
+    dataset = infer_classification(dataset)
+
     print(dataset)
-    # save in csv format
-    dataset.to_csv("./data/Qualtrics_Annotations_formatB_out.csv", index=False)
